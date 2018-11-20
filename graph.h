a81c4f340bd63776dce92abc94625b97f13801aa
Daniel Yan <dyanonlineweb@gmail.com>
2018-11-19 18:57:42 -0800

    Update graph.h
    Correctly uploaded the right graph.h file

--- graph.h
+++ graph.h
@@ -1,61 +1,164 @@
+/**
+* Original made by Pisan
+* Edited by Daniel Yan
+* Pre-conditions on Graph:
+* A graph is made up of vertices and edges.
+* Vertex labels are unique.
+* A vertex can be connected to other vertices via weighted, directed edge.
+* A vertex cannot connect to itself or have multiple edges to the same vertex
+*/
 
-Class Interactions:
+#ifndef ASS3_GRAPHS_GRAPH_H
+#define ASS3_GRAPHS_GRAPH_H
 
-
-Current Implementation:
-Customer
-MovieStore 
-	MovieFactories
-		ComedyFactory
-			Comedy
-		DramaFactory
-			Drama
-		ClassicsFactory
-			Classics
-File Input Reading Input
-1. data4movies.txt - Initialize Map of MovieTypes
-2. data4customers.txt - Initialize Hash Map of Customer (ID to name)
-3. data4commands.txt - Call relevant functions, create Factories & relevant objects per class
-			
-Future Implementation:
-MediaFactories
-	DVDFactories
-		DVD
-	VideoFactories
-		Videos
-	MovieFactories
-		ComedyFactory
-			Comedy
-		DramaFactory
-			Drama
-		ClassicsFactory
-			Classics
-
-Class Implementations:
-MovieStore
-- Hash Map of Customers
-- Map of all MovieTypes
-- Read in all files (Commands, Customers, Movies)
-- Methods for each System Action (overrided in specific subclasses)
-	Borrow
-	Return
-	Inventory
-	History
--Static variable count for MovieStock
-
-MovieFactories
-- Contains create method for each Factory subclass to be overloaded
-- Self-registration method implemented for each Factory subclass
-
-ComedyFactory
-- Override create method from MovieFactory
-- Self-registration
-- Create Comedy class objects
-
-Comedy
-- Initializes a Map to keep track of movies
-- Reads functions from data4commands.txt
+#include <map>
+#include <string>
+#include "vertex.h"
 
 
+class Graph {
+public:
+	/** constructor, empty graph */
+	Graph();
+
+	/** destructor, delete all vertices and edges */
+	~Graph();
+
+	/**
+	* @return total number of vertices
+	* */
+	int NumberOfVertices() const;
+
+	/**
+	* @return total number of edges
+	* */
+	int NumberOfEdges() const;
+
+	/**
+	* @param label
+	* @return number of edges from given vertex, -1 if vertex not found
+	* */
+	int NumberOfEdges(const std::string &label) const;
+
+	/**
+	* Add a vertex to the graph, no duplicates allowed
+	* @param label
+	* @return true if vertex added, false if it already is in the graph
+	* */
+	bool AddVertex(const std::string &label);
+
+	/**
+	* @param label
+	* @return true if vertex is in the graph
+	* */
+	bool HasVertex(const std::string &label) const;
+
+	/**
+	* Get edges of the vertex in a readable format
+	* such as b(10),c(20),d(40)
+	* @param label
+	* @return string representing edges and weights, "" if vertex not found
+	* */
+	std::string GetEdges(const std::string &label) const;
+
+	/**
+	* Add a new edge between start and end vertex
+	* If the vertices do not exist, create them
+	* A vertex cannot connect to itself
+	* or have multiple edges to the same vertex
+	* @param fromLabel
+	* @param toLabel
+	* @param edgeWeight optional, defaults to 0
+	* @return true if successfully connected
+	* */
+	bool Connect(const std::string &fromLabel,
+		const std::string &toLabel, int edgeWeight);
+
+	/**
+	* Remove edge from graph
+	* @param fromLabel starting vertex label
+	* @param edge ending vertex label
+	* @return true if edge successfully deleted
+	* */
+	bool Disconnect(const std::string &fromLabel, const std::string &toLabel);
+
+	/**
+	* Read edges from file
+	* first line of file is an integer, indicating number of edges
+	* each line represents an edge in the form of "string string int"
+	* vertex labels cannot contain spaces
+	* @param filename
+	* @return true if file successfully read
+	* */
+	bool ReadFile(const std::string &filename);
+
+	/**
+	* depth-first traversal starting from given startLabel
+	* @param startLabel starting vertex label
+	* @param visit function to be called on each vertex label
+	* */
+	void DFS(const std::string &startLabel, void visit(const std::string &));
+
+	/** breadth-first traversal starting from startLabel
+	call the function visit on each vertex label */
+	/**
+	* breadth-first traversal starting from given startLabel
+	* @param starting vertex label
+	* @param function to be called on each vertex label
+	* */
+	void BFS(const std::string &startLabel, void visit(const std::string &));
+
+	/**
+	* dijkstra's algorithm to find shortest distance to all other vertices
+	* and the path to all other vertices
+	* Path cost is recorded in the map passed in, e.g. weight["F"] = 10
+	* How to get to vertex is recorded in map passed in, previous["F" = "C"
+	* @param starting vertex label
+	* @param map of <string, int> to record shortest path to vertex
+	* @param map of <string, string> to record previous node in path to vertex
+	* */
+	void Dijkstra(const std::string &startLabel,
+		std::map<std::string, int> &weights,
+		std::map<std::string, std::string> &previous);
+
+private:
+	/** mark all verticies as unvisited */
+	void unvisitVertices();
+
+	/** helper for depthFirstTraversal */
+	void dfsHelper(Vertex *v, void visit(const std::string &));
+
+	/** helper for breadthFirstTraversal */
+	void bfsHelper(Vertex *v, void visit(const std::string &));
+
+	/**
+	* find vertex with given label
+	* @param vertexLabel string as the vertex label
+	* @return pointer to vetex or nullptr if not in graph
+	*/
+	Vertex *findVertex(const std::string &vertexLabel) const;
+
+	/**
+	* create a new vertex,
+	* @param vertexLabel string as the vertex label
+	* @return pointer to the newly created vertex
+	*/
+	Vertex *createVertex(const std::string &vertexLabel);
+
+	/*
+	* sorting function for vector of edges, uses a lambda function
+	* and iterators to determine what to sort
+	* @param string for origin Vertex
+	* @return nothing
+	*/
+	void sortEdges(std::string &fromLabel);
+
+	/** number of edges in graph */
+	int numberOfEdges = 0;
+	
+	/** mapping from vertex label to vertex pointer for quick access */
+	std::map<std::string, Vertex *> vertices;
+};
 
 
+#endif  // ASS3_GRAPHS_GRAPH_H
