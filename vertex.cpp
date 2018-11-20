63722484f33abdddc207b590bd505ec5e24a5721
Daniel Yan <dyanonlineweb@gmail.com>
2018-11-19 18:54:58 -0800

    Update vertex.cpp
    Quick reformatting and code cleanup

--- vertex.cpp
+++ vertex.cpp
@@ -4,8 +4,7 @@
 #include "vertex.h"
 
 //One liner to initialize vertex with a label and visit state
-Vertex::Vertex(string l) : label(l), visited(false) {}
-
+Vertex::Vertex(const string & label) : label(l), visited(false) {}
 
 //Returns the outgoing edge based on the string
 Edge* Vertex::getOutgoingEdge(string l) {
@@ -18,21 +17,3 @@ Edge* Vertex::getOutgoingEdge(string l) {
 	//If outgoing edge is not found
 	return nullptr;
 }
-		
-
-//Unused Getters and setters 
-/*
-vector<Edge *> Vertex::getEdges() const {
-	return allOutgoingEdges;
-}
-string Vertex::getLabel() const {
-	return label;
-}
-
-void Vertex::setVisited(bool state) {
-	visited = state;
-}
-bool Vertex::getVisited() const {
-	return visited;
-}
-*/
